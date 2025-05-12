eda80ad8b5507e967ea26843d6a6998e27b98459
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-05-12 11:29:50 +0200

    style(NewCountryForm): improve form styling and layout for better user experience

--- frontend/src/components/NewCountryForm.tsx
+++ frontend/src/components/NewCountryForm.tsx
@@ -55,29 +55,32 @@ export function NewCountryForm() {
         className="bg-[#F1F1F1] p-4 rounded-md border border-gray-200 flex gap-2 flex-col sm:flex-row"
         onSubmit={handleSubmit}
       >
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mb-2 lg:mb-0">
           {["name", "emoji", "code"].map((field) => (
-            <div key={field} className="flex flex-col gap-2">
+            <div key={field} className="flex flex-col gap-1">
               <label htmlFor={field}>
-                {field.charAt(0).toUpperCase() + field.slice(1)}
+                {field.charAt(0).toUpperCase() + field.slice(1)}{" "}
+                <span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
                 id={field}
                 value={formData[field as keyof typeof formData]}
                 onChange={handleChange}
-                className="bg-white rounded-md p-1 border border-gray-300 w-full max-w-[100%] sm:max-w-[70%] hover:border-gray-400"
+                className="bg-white rounded-xs p-1 border border-gray-400 w-full max-w-[100%] sm:max-w-[70%] hover:border-gray-500"
                 required
               />
             </div>
           ))}
-          <div className="flex flex-col gap-2">
-            <label htmlFor="continentId">Continent</label>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="continentId">
+              Continent <span className="text-red-500">*</span>
+            </label>
             <select
               id="continentId"
               value={formData.continentId}
               onChange={handleChange}
-              className="bg-white rounded-md p-1 border border-gray-300 w-full max-w-[100%] sm:max-w-[70%] cursor-pointer h-[34px] hover:border-gray-400"
+              className="bg-white rounded-xs p-1 border border-gray-400 w-full max-w-[100%] sm:max-w-[70%] cursor-pointer h-[34px] hover:border-gray-500"
               required
             >
               <option value="" disabled selected>
@@ -95,7 +98,7 @@ export function NewCountryForm() {
         <button
           type="submit"
           disabled={loading}
-          className="bg-[#F7146B] text-white p-4 rounded-md hover:bg-[#F7146B]/90 hover:cursor-pointer my-auto disabled:opacity-50 h-15 w-18 ml-auto mt-2 sm:mt-0"
+          className="bg-[#F7146B] text-white p-3 sm:p-4 rounded-md hover:bg-[#F7146B]/90 hover:cursor-pointer my-auto disabled:opacity-50 w-full h-fit sm:h-15 sm:w-18 ml-auto"
         >
           {loading ? <Loader /> : "Add"}
         </button>
