0ca044e8b753880a1f814bc1e120b29020cde8d6
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-05-12 09:27:30 +0200

    style(Header): enhance header styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="header">
-      <h1>Checkpoint : frontend</h1>
-      <Link to="/">Countries</Link>
+    <header className="flex items-center flex-col justify-center gap-4 p-4 text-white bg-[#F7146B]">
+      <h1 className="text-2xl font-bold">Checkpoint : frontend</h1>
+      <Link to="/" className="font-normal">
+        Countries
+      </Link>
     </header>
   );
 }
